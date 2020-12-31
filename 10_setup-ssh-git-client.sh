97dc57e9d9b634dfec953307001f993489d616b3
angelszymczak <juanangelszymczak@gmail.com>
2020-12-30 17:05:08 -0800

    Setup SSH Git client

---
+++ 10_setup-ssh-git-client.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ssh-keygen -t rsa -b 4096 -C "your@email.com"
+# Generating public/private rsa key pair.
+# Enter file in which to save the key (/home/angel/.ssh/id_rsa): /home/angel/.ssh/github
+# Enter passphrase (empty for no passphrase): 
+# Enter same passphrase again: 
+# Your identification has been saved in github.
+# Your public key has been saved in github.pub.
+
+# Start SSH Agent service
+eval "$(ssh-agent -s)"
+# Agent pid 4723
+
+ssh-add .ssh/github
+# Identity added: .ssh/github (.ssh/github)
+
+# Let's install secure clipboard tool
+sudo apt-get install xclip -y
+
+# Copy the pulic key content and add it to GitHub SSH settings
+xclip -sel clip < ~/.ssh/github.pub
+
