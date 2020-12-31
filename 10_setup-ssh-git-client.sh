30c4d741728115d48eceaa2108ec0ed0f7f7ef0a
Angel Szymczak <juanangelszymczak@gmail.com>
2020-12-31 11:18:33 -0300

    Parametrize email on git ssh setup

--- 10_setup-ssh-git-client.sh
+++ 10_setup-ssh-git-client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh-keygen -t rsa -b 4096 -C "your@email.com"
+ssh-keygen -t rsa -b 4096 -C "$1"
 # Generating public/private rsa key pair.
 # Enter file in which to save the key (/home/angel/.ssh/id_rsa): /home/angel/.ssh/github
 # Enter passphrase (empty for no passphrase): 
