8ffe2261d3ffed417c1690181077caa9639713ca
angelszymczak <juanangelszymczak@gmail.com>
2020-12-30 16:21:57 -0800

    AWS CLI v2 client

---
+++ 06_aws_cli_v2_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install the AWS CLI version 2 on Linux
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+rm -rf aws*
+aws --version
